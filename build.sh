a9f812483d957b4d01f005e8e3d9b5f95bd9cd13
Christopher J. Davies <cdavies@itrsgroup.com>
2018-07-03 14:21:33 +0100

    Correct pubish

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Running build"
+mkdir build_reports
+echo "Hi" >> build_reports/build.html
 mkdir -p ${BRANCH_DIR}/build/${BUILD_TYPE}
 touch ${BRANCH_DIR}/build/${BUILD_TYPE}/gateway2
 ls -al ${BRANCH_DIR}/build/${BUILD_TYPE}
