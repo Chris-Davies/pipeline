f913fe4932a80b275e7eae0f1208ce78a1f1ef50
Christopher J. Davies <cdavies@itrsgroup.com>
2018-07-03 14:34:05 +0100

    attempt 3 at script

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ echo "Hi" >> build_reports/build.html
 mkdir -p ${BRANCH_DIR}/build/${BUILD_TYPE}
 touch ${BRANCH_DIR}/build/${BUILD_TYPE}/gateway2
 ls -al ${BRANCH_DIR}/build/${BUILD_TYPE}
-echo "1:1:1" >> {BRANCH_DIR}/build/${BUILD_TYPE}/build-result.txt
+echo "1:1:1" >> ${BRANCH_DIR}/build/${BUILD_TYPE}/build-result.txt
