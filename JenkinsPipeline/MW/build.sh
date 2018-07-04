565f8b6d1bffb672864133cedc26f294822cd6fe
Christopher J. Davies <cdavies@itrsgroup.com>
2018-07-04 17:20:10 +0100

    Removed Build_dir

--- JenkinsPipeline/MW/build.sh
+++ JenkinsPipeline/MW/build.sh
@@ -2,7 +2,7 @@
 echo "Running build"
 mkdir build_reports
 echo "Hi" >> build_reports/build.html
-mkdir -p ${BRANCH_DIR}/build/${BUILD_TYPE}
-touch ${BRANCH_DIR}/build/${BUILD_TYPE}/gateway2
-ls -al ${BRANCH_DIR}/build/${BUILD_TYPE}
-echo "0:0:1" >> ${BRANCH_DIR}/build/${BUILD_TYPE}/build-result.txt
+mkdir -p build/${BUILD_TYPE}
+touch build/${BUILD_TYPE}/gateway2
+ls -al build/${BUILD_TYPE}
+echo "0:0:0" >> build/${BUILD_TYPE}/build-result.txt
