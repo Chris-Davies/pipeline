bc80549d691a7d61fa619b3d7981e6f6f1210bb0
Christopher J. Davies <cdavies@itrsgroup.com>
2018-07-13 14:02:24 +0100

    JAVA_HOME

--- JenkinsPipeline/MW/build.sh
+++ JenkinsPipeline/MW/build.sh
@@ -2,6 +2,7 @@
 echo "Running build"
 mkdir build_reports
 echo "Hi" >> build_reports/build.html
+echo "JAVA_HOME = $JAVA_HOME"
 mkdir -p build/${BUILD_TYPE}
 touch build/${BUILD_TYPE}/gateway2
 ls -al build/${BUILD_TYPE}
